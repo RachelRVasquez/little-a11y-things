2fd91265d86a0ce9813f2128fdae2450a423c39c
unknown <rachelrvasquez@gmail.com>
2025-11-13 13:39:13 -0500

    installed syntax highlighter, was starting to work on content, but will style first

--- src/components/DialogExample/DialogInfo.tsx
+++ src/components/DialogExample/DialogInfo.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 function DialogInfo(){
     return(
         <div className="dialog-info">
@@ -5,13 +6,45 @@ function DialogInfo(){
                 <h2>
                     What is it?
                 </h2>
-                <p>Write short description of what this code is meant to display/explain</p>
+                <p>A
+                    <Link to={{
+                    pathname: "https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dialog/" }}
+                     target="_blank">dialog element</Link>
+                    is the semantic way to create a modal dialog or non-modal dialog. It comes with built attributes and functions
+                    that can be used to control its behavior without having to write Javascript for common sense things like opening
+                    and closing the dialog.
+                </p>
+                <p>It even comes with a backdrop that covers all the content behind the dialog. No more fussing
+                    with tricky z-index's and styles - unless you want to! The backdrop can be styled via CSS.</p>
             </section>
             <section>
                 <h2>
-                    Modal or Dialog, does it matter?
+                    What the heck is a modal dialog and a non-modal dialog?
                 </h2>
-                <p>Yes. Link to article about when to use either.</p>
+                <p>A "modal" and a "dialog" are not the same thing. Surprised? Same bro, blew my mine when I learned this.</p>
+                <p>The <Link to={{
+                    pathname: "https://www.a11y-collective.com/blog/modal-vs-dialog/"}}
+                    target="_blank">A11y Collection</Link>
+                 has a nice in-depth review of the differences, but here's the gist of it:</p>
+                <h3>Modal dialogs</h3>
+                <p>Modal dialogs block interaction with the rest of the page. For UX reasons, you want to take care with these. Here
+                are a few examples of ways you can use a modal dialog.</p>
+                <ul>
+                    <li>Confirming destructive actions like when someone wants to delete data that's irreversible.</li>
+                    <li>Collecting required payment details.</li>
+                    <li>Critical warnings and alerts.</li>
+                    <li>For legal notifications that need to be acknowledged or agreed to by the end-user.</li>
+                </ul>
+                <p>You want to avoid using a modal dialog that pops up as soon as a user arrives to the site for something
+                like a newsletter signup.</p>
+                <h3>Non-modal dialog</h3>
+                <p>Non-modal dialogs provide info or options without preventing interaction with the content behind it. A few
+                examples of how you'd use this in the wild:</p>
+                <ul>
+                    <li>Newsletter signups!</li>
+                    <li>Help text or tool tips.</li>
+                    <li>Optional settings and preferences.</li>
+                </ul>
             </section>
             <section>
                 <h2>ARIA attributes</h2>
--- src/components/DialogExample/DialogVisualMode.tsx
+++ src/components/DialogExample/DialogVisualMode.tsx
@@ -1,6 +1,9 @@
 function DialogVisualMode(){
     return(
-        <div>visual mode here</div>
+        <div className="visual-mode-content">
+            <p>Here's a fully functioning button element that triggers a bonafide dialog! Give it a go.</p>
+            <p>If you want to see the code under the hood, go ahead and click that "Code Mode" tab.</p>
+        </div>
     );
 }
 
