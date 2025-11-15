a2b6cd2ff4d049223257ce48fb90e7e4f91b3e30
unknown <rachelrvasquez@gmail.com>
2025-11-15 00:17:52 -0500

    updated fav icon, still styling, working on content for dialog

--- src/components/DialogExample/DialogInfo.tsx
+++ src/components/DialogExample/DialogInfo.tsx
@@ -3,6 +3,7 @@ function DialogInfo(){
     return(
         <>
             <section>
+                <hr/>
                 <h3>
                     What is it?
                 </h3>
@@ -12,7 +13,7 @@ function DialogInfo(){
                     that can be used to control its behavior without having to write Javascript for common sense things like opening
                     and closing the dialog.
                 </p>
-                <p>It even comes with a backdrop that covers all the content behind the dialog. No more fussing
+                <p className="tagline">It even comes with a backdrop that covers all the content behind the dialog. No more fussing
                     with tricky z-index's and styles - unless you want to! The backdrop can be styled via CSS.</p>
             </section>
             <section>
@@ -23,25 +24,25 @@ function DialogInfo(){
                 <p>The <Link to={{
                     pathname: "https://www.a11y-collective.com/blog/modal-vs-dialog/"}}
                     target="_blank">A11y Collection</Link> has a nice in-depth review of the differences, but here's the gist of it:</p>
-                <h3>Modal dialogs</h3>
-                <p>Modal dialogs block interaction with the rest of the page. For UX reasons, you want to take care with these. Here
-                are a few examples of ways you can use a modal dialog.</p>
+                <h4>Modal dialogs</h4>
+                <p>Modal dialogs block interaction with the rest of the page.
+                    Here are a few examples of when to use a modal dialog.</p>
                 <ul>
-                    <li>Confirming destructive actions like when someone wants to delete data that's irreversible.</li>
+                    <li>Confirming destructive actions like before deleting data that's irreversible.</li>
                     <li>Collecting required payment details.</li>
                     <li>Critical warnings and alerts.</li>
-                    <li>For legal notifications that need to be acknowledged or agreed to by the end-user.</li>
+                    <li>For legal notifications that need to be acknowledged or agreed to.</li>
                 </ul>
-                <p>You want to avoid using a modal dialog that pops up as soon as a user arrives to the site for something
-                like a newsletter signup.</p>
-                <h3>Non-modal dialog</h3>
-                <p>Non-modal dialogs provide info or options without preventing interaction with the content behind it. A few
-                examples of how you'd use this in the wild:</p>
+                <p className="tip"><b>Tip:</b> Use the <code>showModal()</code> function to block page interaction.</p>
+                <h4>Non-modal dialog</h4>
+                <p>Non-modal dialogs provide info or options without preventing interaction with the content behind it.
+                    Here are a few examples of when to use a non-modal dialog:</p>
                 <ul>
                     <li>Newsletter signups!</li>
                     <li>Help text or tool tips.</li>
                     <li>Optional settings and preferences.</li>
                 </ul>
+                <p className="tip"><b>Tip:</b> Use the <code>show()</code> function so you don't block interaction.</p>
             </section>
             <section>
                 <h3>ARIA attributes</h3>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,23 +6,28 @@ export default function Home() {
             <p className="tagline">Welcome to my little collection of accessible things!</p>
 
             <p>Here you'll find accessible code snippets, be able to physically see the code in action, and learn
-                from documentation that is easier on the eyes.s</p>
+                how the code works with no-fluff documentation.</p>
 
             <p>I hope these small, or shall I say <i>little</i>, pieces of knowledge are easier
-                for developers to digest. I'll be adding more pages over time.</p>
-
-            <p>(I also wanted an excuse to use React, so here you go! Enjoy!)</p>
+                for developers to digest.</p>
+            <hr/>
 
             <section id="definitions">
-                <h3>Glossary:</h3>
-                <b>Semantics:</b>
-                <p> Why are semantics important? It's how search engines and assistive technologies
+                <h3>Glossary</h3>
+                <p className="glossary-word"><b>A11y</b></p>
+                <p>A11y is an abbreviation for accessibility, the number 11 is for the letters between "a" and "y".</p>
+
+                <p className="glossary-word"><b>ARIA</b></p>
+                <p>A set of attributes that make our code more accessible, primarily for assistive technologies.</p>
+
+                <p className="glossary-word"><b>Semantics</b></p>
+                <p>It's how search engines and assistive technologies
                     understand the structure of a site and it's content. It also shows that you, as
                     a developer, have a foundational understanding of how HTML works.
                 </p>
                 <p>Using semantic elements is like building a house. There's a reason why you have different nails, and screws.
-                    Sure, they can look similar. Sure, you can use nails for everything. But nails aren't intended for
-                    certain things. Using nails in a way they're not intended then weakens the overall structure of
+                    Sure, they can look similar. Sure, you can use nails for everything. But nails serve a specific purpose.
+                    Using nails in a way they're not intended weakens the overall structure of
                     your house. Might even make your house fall apart.</p>
                 <p className="tagline">"If you want your house aka your site to last, write code semantically."</p>
             </section>
