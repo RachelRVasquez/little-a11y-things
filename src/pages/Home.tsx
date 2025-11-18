54cc41330d779828ce79a2f5c783d1cbbac98d6d
unknown <rachelrvasquez@gmail.com>
2025-11-17 22:08:20 -0500

    working on buttons visual mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <section id="home">
-            <h2>It's the little things that matter!</h2>
+            <p className="h2-style">It's the little things that matter!</p>
 
             <p className="tagline">Welcome to my little collection of accessible things!</p>
 
@@ -14,9 +14,9 @@ export default function Home() {
             <hr/>
 
             <section id="definitions">
-                <h3>Glossary</h3>
+                <h2>Glossary</h2>
                 <p className="glossary-word"><b>A11y</b></p>
-                <p>A11y is an abbreviation for accessibility, the number 11 is for the letters between "a" and "y".</p>
+                <p>A11y is an abbreviation for accessibility. The number 11 is for the letters between "a" and "y".</p>
 
                 <p className="glossary-word"><b>ARIA</b></p>
                 <p>A set of attributes that make our code more accessible, primarily for assistive technologies.</p>
@@ -26,7 +26,7 @@ export default function Home() {
                     understand the structure of a site and it's content. It also shows that you, as
                     a developer, have a foundational understanding of how HTML works.
                 </p>
-                <p>Using semantic elements is like building a house. There's a reason why you have different nails, and screws.
+                <p>Using semantic elements in our code is like building a house. There's a reason why you have different nails, and screws.
                     Sure, they can look similar. Sure, you can use nails for everything. But nails serve a specific purpose.
                     Using nails in a way they're not intended weakens the overall structure of
                     your house. Might even make your house fall apart.</p>
