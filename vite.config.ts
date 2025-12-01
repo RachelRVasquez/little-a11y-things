2a9e36f79aaf5a5cd9339d487f15135bb029ef55
unknown <rachelrvasquez@gmail.com>
2025-12-01 12:21:55 -0500

    removed deploy script, installed gh-pages instead, added deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/little-a11y-things/',
+    base: '/little-a11y-things',
     plugins: [react()],
 })
